1e924f7de13ca2c65c47b39441fcfc7039333710
Njodsa <31994505+Njodsa@users.noreply.github.com>
2020-05-15 22:48:24 +0300

    style-design website

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,7 @@ require('./bootstrap');
 window.Vue = require('vue');
 import router from './router'
 Vue.component('paginate', require('vuejs-paginate'));
+Vue.component('the-navigation', require('./components/theNavigation.vue').default);
 
 const app = new Vue({
     el: '#app',
