65c80cbe684b9f70621216be49947211aaeb5156
Njodsa <31994505+Njodsa@users.noreply.github.com>
2020-05-16 00:06:18 +0300

    edit-book modul name

--- app/book.php
+++ app/book.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class book extends Model
+class Book extends Model
 {
   protected $fillable = ['name', 'authorname', 'isbn','genres','price'];
 }
